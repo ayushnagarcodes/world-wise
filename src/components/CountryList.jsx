21d2a211ee1cc4fac5417a3a0e9109838b8e90ee
Ayush Nagar <ayushnagar15tfe@gmail.com>
2023-11-30 00:33:19 +0530

    Added project and implemented navigation using React Router

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,31 @@
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import CountryItem from "./CountryItem";
+import Message from "./Message";
+
+function CountryList({ cities, isLoading }) {
+    if (isLoading) return <Spinner />;
+
+    if (!cities.length)
+        return (
+            <Message message="Add your first city by clicking on a city on the map" />
+        );
+
+    const countries = cities.reduce(
+        (arr, city) =>
+            arr.map((el) => el.country).includes(city.country)
+                ? [...arr]
+                : [...arr, { country: city.country, emoji: city.emoji }],
+        []
+    );
+
+    return (
+        <ul className={styles.countryList}>
+            {countries.map((country, i) => (
+                <CountryItem country={country} key={i} />
+            ))}
+        </ul>
+    );
+}
+
+export default CountryList;
